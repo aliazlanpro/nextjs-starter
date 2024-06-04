5a57a4b495740557e1d54b827281780757ba8b71
Ali Azlan <136848358+aliazlanpro@users.noreply.github.com>
2024-06-04 21:43:35 +0500

    fix: progress bar in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { cn } from "@/utils/shadcn";
 import { ThemeProvider } from "@/providers/theme-provider";
 import { Toaster } from "@/components/ui/sonner";
+import { ProgressBar } from "@/components/global/progress-bar";
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
 
@@ -31,8 +32,10 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          {children}
-          <Toaster />
+          <ProgressBar className="fixed top-0 h-1 z-40 dark:h-0.5 bg-primary">
+            {children}
+            <Toaster />
+          </ProgressBar>
         </ThemeProvider>
       </body>
     </html>
